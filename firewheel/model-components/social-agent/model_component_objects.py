7a9623a9168a7cc70493bfa0ba041fe6656e5a96
Thien-Nam Dinh <thidinh@sandia.gov>
2025-07-05 23:46:19 -0700

    initial commit

---
+++ firewheel/model-components/social-agent/model_component_objects.py
@@ -0,0 +1,53 @@
+"""This module contains all necessary Model Component Objects for synchronic_web.social_agent."""
+import json
+
+from firewheel.control.experiment_graph import require_class
+from linux.ubuntu2204 import Ubuntu2204Server
+from utilities.tools import Utilities
+
+
+@require_class(Utilities)
+@require_class(Ubuntu2204Server)
+class SocialAgent:
+
+    def __init__(self, journal, secret, periodicity, size, activity, peers):
+        self.journal = journal
+        self.secret = secret
+        self.periodicity = periodicity
+        self.size = size
+        self.activity = activity
+        self.peers = peers
+
+        self.add_docker()
+        self.run_agent()
+        self.increase_resources()
+
+    def run_agent(self):
+        args = ' '.join([
+            f'-e JOURNAL={self.journal}',
+            f'-e SECRET={self.secret}',
+            f'-e PERIODICITY={self.periodicity}',
+            f'-e SIZE={self.size}',
+            f'-e ACTIVITY={self.activity}',
+            f'-v /home/ubuntu/peers.json:/srv/peers.json',
+            f'--net=host',
+            f'--name social-agent',
+        ])
+
+        self.drop_file(
+            -32,
+            '/home/ubuntu/social-agent-image.tar',
+            'social-agent-image.tar',
+        )
+        self.drop_content(-31, '/home/ubuntu/peers.json', json.dumps(self.peers))
+        self.run_executable(-30, 'docker', 'load -i /home/ubuntu/social-agent-image.tar')
+        self.run_executable(1, 'docker', f'run {args} social-agent')
+
+    def increase_resources(self):
+        num_cores = 2
+        num_threads = 2
+        if self.vm['vcpu']['cores'] < num_cores:
+            self.vm['vcpu']['cores'] = num_cores
+        if self.vm['vcpu']['threads'] < num_threads:
+            self.vm['vcpu']['threads'] = num_threads
+        self.vm['mem'] = 1024 * 4
---
+++ firewheel/model-components/social-agent/plugin.py
@@ -0,0 +1,74 @@
+import random
+
+from firewheel.control.experiment_graph import AbstractPlugin, Vertex
+from synchronic_web.journal import Journal
+from synchronic_web.social_agent import SocialAgent
+
+from base_objects import Switch, VMEndpoint
+from netaddr import IPNetwork
+
+random.seed(0)
+
+class Plugin(AbstractPlugin):
+    """synchronic_web.social_agent plugin documentation."""
+
+    def run(self, connectivity='2', size='32', activity='0'):
+        connectivity=int(connectivity)
+        size = int(size)
+        activity = int(activity)
+
+        journals = [
+            v.name for v in self.g.get_vertices() if v.is_decorated_by(Journal)
+        ]
+
+        peers = {
+            x: random.sample(
+                [y for y in journals if y != x],
+                min(connectivity, len(journals) - 1),
+            )
+            for x in journals
+        } if len(journals) > 1 else {}
+
+        for vertex in list(self.g.get_vertices()):
+            if vertex.is_decorated_by(Journal):
+                journal = vertex
+                index = int(journal.name.split('.', 1)[0].rsplit('-', 1)[-1])
+
+                # Create the journal/agent network
+                journal_agent_net = IPNetwork('10.0.0.0/16')
+                ips = journal_agent_net.iter_hosts()
+                journal_agent_sw = Vertex(self.g)
+                journal_agent_sw.decorate(
+                    Switch, init_args=[f'journal-agent-sw-{index}'])
+
+                # Connect the journal to the agent on a private network
+                journal_ip = next(ips)
+                journal.connect(journal_agent_sw, journal_ip,
+                                journal_agent_net.netmask)
+
+                # Create the agent
+                agent_name = f'agent-{index}.net'
+                agent = Vertex(self.g, agent_name)
+                agent.decorate(
+                    SocialAgent,
+                    init_args=[
+                        journal.name,
+                        journal.secret,
+                        journal.periodicity,
+                        size,
+                        activity,
+                        peers,
+                    ],
+                )
+                
+                agent.run_executable(
+                    -10,
+                    'echo',
+                    arguments=[f'"{journal_ip} {journal.name}"', '>>', '/etc/hosts'],
+                    vm_resource=False,
+                )
+
+                # Connect the agent to both the journal on a private network,
+                # and to the "environment" on a "simulation" network
+                agent.connect(journal_agent_sw, next(ips),
+                              journal_agent_net.netmask)
